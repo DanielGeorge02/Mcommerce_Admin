9791ad6bb3347a855ac033163095c669c16e07c9
deepakn7502 <dostmeradeepak@gmail.com>
2023-09-21 16:12:11 +0530

    code package changed

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// import admin from "firebase-admin";
-import { initializeApp, applicationDefault } from "firebase-admin/app";
-import { getMessaging } from "firebase-admin/messaging";
-import express, { json } from "express";
-import cors from "cors";
-process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-initializeApp({
-  credential: applicationDefault(),
-  projectId: "cathartic-gofer",
-});
+var FCM = require("fcm-node");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const serverKey = process.env.SERVER_KEY;
+const fcm = new FCM(serverKey);
 
 const app = express();
+
 app.use(express.json());
 
 app.use(
@@ -26,34 +22,33 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send({ message: "API is working successfully" });
-});
-
 app.post("/send", (req, res) => {
-  const message = {
+  var message = {
+    to: req.body.receiverToken,
     notification: {
       title: req.body.title,
-      body: req.body.message,
+      body: req.body.body,
     },
-    token: req.body.fcmToken,
+
+    data: {
+      //you can send only notification or only data(or include both)
+      title: "ok cdfsdsdfsd",
+      body: '{"name" : "okg ooggle ogrlrl","product_id" : "123","final_price" : "0.00035"}',
+    },
   };
 
-  getMessaging()
-    .send(message)
-    .then((response) => {
-      res.status(200).json({
-        message: "Successfully sent message",
-      }),
-        console.log("Successfully sent message:", response);
-    })
-    .catch((error) => {
-      res.status(400);
-      res.send(error);
-      console.log("Error sending message:", error);
-    });
+  fcm.send(message, function (err, response) {
+    if (err) {
+      console.log("Something has gone wrong!" + err);
+      console.log("Respponse:! " + response);
+    } else {
+      // showToast("Successfully sent with response");
+      console.log("Successfully sent with response: ", response);
+      res.json(response);
+    }
+  });
 });
 
 app.listen(8000, (req, res) => {
-  console.log("Server running on the port 8000");
+  console.log("listening to port 8000");
 });
