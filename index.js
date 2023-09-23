f56397e5fc454edf48c53b1936e75a9a8e78e6ec
deepakn7502 <dostmeradeepak@gmail.com>
2023-09-23 08:38:17 +0530

    Past date condition added

--- index.js
+++ index.js
@@ -55,35 +55,36 @@ app.post("/schedule", (req, res) => {
   const now = new Date();
   const targetTime = new Date(req.body.time);
   const millisecondsToWait = targetTime.getTime() - now;
-  console.log(now, targetTime);
-  console.log(millisecondsToWait);
+  if (millisecondsToWait < 0) {
+    res.status(400).json({ message: "Select a future date and time" });
+  } else {
+    setTimeout(() => {
+      // Your code here
 
-  setTimeout(() => {
-    // Your code here
+      var message = {
+        to: req.body.fcmToken,
+        notification: {
+          title: req.body.title,
+          body: req.body.body,
+        },
 
-    var message = {
-      to: req.body.fcmToken,
-      notification: {
-        title: req.body.title,
-        body: req.body.body,
-      },
+        data: {
+          title: "ok cdfsdsdfsd",
+          body: '{"name" : "okg ooggle ogrlrl","product_id" : "123","final_price" : "0.00035"}',
+        },
+      };
 
-      data: {
-        title: "ok cdfsdsdfsd",
-        body: '{"name" : "okg ooggle ogrlrl","product_id" : "123","final_price" : "0.00035"}',
-      },
-    };
-
-    fcm.send(message, function (err, response) {
-      if (err) {
-        console.log("Something has gone wrong!" + err);
-        console.log("Respponse:! " + response);
-      } else {
-        console.log("Successfully sent with response: ", response);
-      }
-    });
-  }, millisecondsToWait);
-  res.send("Message Scheduled");
+      fcm.send(message, function (err, response) {
+        if (err) {
+          console.log("Something has gone wrong!" + err);
+          console.log("Respponse:! " + response);
+        } else {
+          console.log("Successfully sent with response: ", response);
+        }
+      });
+    }, millisecondsToWait);
+    res.json({ message: "Message scheduled successfully" });
+  }
 });
 
 app.listen(8000, (req, res) => {
