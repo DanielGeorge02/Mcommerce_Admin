719afdc6ad8eabe29569784bac7c39cdb1407a9f
deepakn7502 <dostmeradeepak@gmail.com>
2023-09-25 13:54:36 +0530

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ const url = window.location.hostname;
 
 const api = axios.create({
   baseURL: `http://${url}:8000/api/`,
+  // baseURL: `https://message-3pso.onrender.com/api/`,
   // baseURL: `https://bharathwaj.pythonanywhere.com/`,
 });
 
--- frontend/src/Pages/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard.js
@@ -41,26 +41,144 @@ const Dashboard = ({ api }) => {
     console.table(allUsers, users, doctors, vendors);
   }, [allUsers]);
   return (
-    <div>
+    <div className="grid w-fit">
       <button
         onClick={() => {
           getUsers();
         }}
-        className="bg-blue-600 px-4 py-2 m-6 outline-none border-none"
+        className="bg-blue-600 w-48 px-4 py-2 m-6 outline-none border-none rounded-xl"
       >
         Get Users
       </button>
 
-      <h1>Doctors</h1>
-      {users?.map((user) => {
-        return (
-          <table>
-            <thead>
-              <tr>Name</tr>
-            </thead>
-          </table>
-        );
-      })}
+      <section className="grid gap-5">
+        <h1 className="font-bold text-3xl">Doctors:</h1>
+        <table className=" ">
+          <thead>
+            <tr className="grid grid-cols-6 place-items-center">
+              <th className="w-full border border-black">Name</th>
+              <th className="w-full border border-black">Phone</th>
+              <th className="w-full border border-black">Register Number</th>
+              <th className="w-full border border-black">Specialization</th>
+              <th className="w-full border border-black">Status</th>
+              {/* <th className="w-full border border-black">{""}</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {doctors?.map((user) => {
+              return (
+                <tr className="grid grid-cols-6">
+                  <td className="w-full border border-black">{user.name}</td>
+                  <td className="w-full border border-black">{user.phoneNo}</td>
+                  <td className="w-full border border-black">{user.reg_no}</td>
+                  <td className="w-full border border-black">
+                    {user.specialization}
+                  </td>
+                  <td className="w-full border border-black">{user.status}</td>
+                  <td className="grid  grid-cols-2 gap-5 px-5">
+                    <button
+                      onClick={() => {
+                        api.put("/users", {
+                          phoneNo: user.phoneNo,
+                          status: "verified",
+                        });
+                      }}
+                      className="bg-lime-500 px-4 py-2  outline-none border-none rounded-xl"
+                    >
+                      Approve
+                    </button>
+                    <button
+                      onClick={() => {
+                        api.put("/users", {
+                          phoneNo: user.phoneNo,
+                          status: "waiting",
+                        });
+                      }}
+                      className="bg-red-600 px-4 py-2  outline-none border-none rounded-xl"
+                    >
+                      Deny
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+      {/* <section className="grid gap-5">
+        <h1 className="font-bold text-3xl">Vendors</h1>
+        <table className=" ">
+          <thead>
+            <tr className="grid grid-cols-6 place-items-center">
+              <th className="w-full border border-black">Name</th>
+              <th className="w-full border border-black">Phone</th>
+              <th className="w-full border border-black">GST Number</th>
+              <th className="w-full border border-black">Specialization</th>
+              <th className="w-full border border-black">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vendors?.map((user) => {
+              return (
+                <tr className="grid grid-cols-6">
+                  <td className="w-full border border-black">{user.name}</td>
+                  <td className="w-full border border-black">{user.phoneNo}</td>
+                  <td className="w-full border border-black">{user.gst_no}</td>
+                  <td className="w-full border border-black">
+                    {user.specialization}
+                  </td>
+                  <td className="w-full border border-black">{user.status}</td>
+                  <td className="grid  grid-cols-2 gap-5 px-5">
+                    <button className="bg-lime-500 px-4 py-2  outline-none border-none rounded-xl">
+                      Approve
+                    </button>
+                    <button className="bg-red-600 px-4 py-2  outline-none border-none rounded-xl">
+                      Deny
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+      <section className="grid gap-5">
+        <h1 className="font-bold text-3xl">Doctors</h1>
+        <table className=" ">
+          <thead>
+            <tr className="grid grid-cols-6 place-items-center">
+              <th className="w-full border border-black">Name</th>
+              <th className="w-full border border-black">Phone</th>
+              <th className="w-full border border-black">Register Number</th>
+              <th className="w-full border border-black">Specialization</th>
+              <th className="w-full border border-black">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {doctors?.map((user) => {
+              return (
+                <tr className="grid grid-cols-6">
+                  <td className="w-full border border-black">{user.name}</td>
+                  <td className="w-full border border-black">{user.phoneNo}</td>
+                  <td className="w-full border border-black">{user.reg_no}</td>
+                  <td className="w-full border border-black">
+                    {user.specialization}
+                  </td>
+                  <td className="w-full border border-black">{user.status}</td>
+                  <td className="grid  grid-cols-2 gap-5 px-5">
+                    <button className="bg-lime-500 px-4 py-2  outline-none border-none rounded-xl">
+                      Approve
+                    </button>
+                    <button className="bg-red-600 px-4 py-2  outline-none border-none rounded-xl">
+                      Deny
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section> */}
     </div>
   );
 };
