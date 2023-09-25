f20c521bf751770b88dfcd7f355359cbf91fa988
DanielGeorge02 <danielgeorge70120@gmail.com>
2023-09-25 21:57:51 +0530

    pull

---
+++ frontend/src/Pages/Random/Random.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Random = () => {
+  return (
+    <div className="">
+      <img src="../../assets/Desktop - 2.png"></img>
+    </div>
+  );
+};
+
+export default Random;
